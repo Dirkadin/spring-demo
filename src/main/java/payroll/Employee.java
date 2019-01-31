977ecd5b4a063ca0481ee0d7b70eeea3525cce01
Dirk <no@no.com>
2019-01-30 20:36:48 -0600

    Added Employee Class

---
+++ src/main/java/payroll/Employee.java
@@ -0,0 +1,22 @@
+package payroll;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data //Lombok annotation.  Creates all getters, setters, equals, hash, and toString
+@Entity //JPA annotation.  Makes object ready to store in JPA-based data store
+public class Employee {
+	
+	private @Id @GeneratedValue Long id; //Key and database generated value
+	private String name;
+	private String role;
+	
+	Employee(String name, String role) {
+		this.name = name;
+		this.role = role;
+	}
+
+}
